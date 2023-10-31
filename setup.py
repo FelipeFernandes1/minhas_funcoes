38f97a04172908f51c60d3d9f4424205b65ccd26
Luiz Felipe Fernandes <85578441+FelipeFernandes1@users.noreply.github.com>
2023-10-31 17:49:11 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/15hL6Yr9aUwd_nG_l4DjRAfMas1fp6Pha
 """
 
+!pip install --upgrade pip setuptools
 from setuptools import setup
 
 setup(
